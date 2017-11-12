8f101cf895ed850259819f4dd4373ec425551b22
dmkskn <dmksknn@gmail.com>
2017-11-12 10:28:34 +0300

    add Stream

--- sight/_streams.py
+++ sight/_streams.py
@@ -2,4 +2,70 @@
 
 
 class Stream:
-    pass
+    """A base class of the streams."""
+    def __init__(self, raw):
+        self._raw = raw  # it's json response from ffprobe
+        self.metadata = self._raw.get("tags", {})
+
+    @property
+    def index(self):
+        """The stream index in the container."""
+        return self._raw["index"]
+
+    @property
+    def codec(self):
+        """The stream codec."""
+        return self._raw["codec_name"]
+
+    @property
+    def type(self):
+        """The common type of the stream."""
+        return self._raw["codec_type"]
+
+    @property
+    def is_default(self):
+        """Specifies whether the stream is the default stream"""
+        return self._raw["disposition"]["default"] == 1
+
+    @is_default.setter
+    def is_default(self, value: bool):
+        """Property setter for self.is_default."""
+        self._raw["disposition"]["default"] = 1 if value else 0
+
+
+
+class VideoStream(Stream):
+    """A video stream."""
+    def __init__(self, raw):
+        Stream.__init__(self, raw)
+        raise NotImplementedError
+
+
+
+class AudioStream(Stream):
+    """An audio stream."""
+    def __init__(self, raw):
+        Stream.__init__(self, raw)
+        raise NotImplementedError
+
+
+
+class SubtitleStream(Stream):
+    """A subtitle stream."""
+    def __init__(self, raw):
+        Stream.__init__(self, raw)
+        raise NotImplementedError
+
+
+
+class DataStream(Stream): # ?
+    def __init__(self, raw):
+        Stream.__init__(self, raw)
+        raise NotImplementedError
+
+
+
+class ImageStream(Stream): # ?
+    def __init__(self, raw):
+        Stream.__init__(self, raw)
+        raise NotImplementedError
