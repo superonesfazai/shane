e2931cdd8f50cf2063d0b90466c2c6c8b7b4e7be
dmkskn <dmksknn@gmail.com>
2017-12-18 10:05:51 +0300

    open function

--- sight/_api.py
+++ sight/_api.py
@@ -1,7 +1,24 @@
-
+import os
+from ._utils import (
+    call_format, call_streams, 
+    call_chapters, make_stream, make_container,
+    )
 
 __all__ = ['open']
 
 
 def open(path):
-    raise NotImplementedError
+    if not os.path.exists(path):
+        raise ValueError # TODO Error
+    format_info = call_format(path)
+    streams_info = list(call_streams(path))
+    if len(streams_info) > 1:
+        # It is a Container object
+        chapters_info = list(call_chapters(path))
+        streams_info = [make_stream(s) for s in streams_info]
+        return make_container(format_info, chapters_info, streams_info)
+    else:
+        # It is a Stream object
+        one_stream_info = streams_info.pop(0)
+        one_stream_info.update(format_info)
+        return make_stream(one_stream_info)
