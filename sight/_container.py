fe3b837e371dd9294700673e148fd77f261b32f6
dmkskn <dmksknn@gmail.com>
2017-12-25 16:35:10 +0300

    add save method to Container

--- sight/_container.py
+++ sight/_container.py
@@ -82,4 +82,72 @@ class Container:
     
     def save(self): # TODO
         """Saves all changes"""
-        raise NotImplementedError
+        def _generate_input_paths(inputs):
+            return [input_file._raw["default_filename"] for input_file in inputs]
+
+        def _generate_input_options(inputs):
+            result = []
+            for x in inputs:
+                if isinstance(x, Container):
+                    result += [[]]
+                else:
+                    # streams from another container or outer streams
+                    if x._codec_if_convert_to[self.extention] == "copy":
+                        result += [[]]
+                    else:
+                        result += [
+                            [   # -[v|a|s|d]codec copy|option
+                                f"-{x.codec_type[0]}codec",
+                                x._codec_if_convert_to[self.extention],
+                            ]
+                        ]
+            return result
+
+        def _input_paths_and_their_options(inputs):
+            result = []
+            input_options = _generate_input_options(inputs)
+            input_paths = _generate_input_paths(inputs)
+            for options, path in zip(input_options, input_paths):
+                result += options  # options are list
+                result += ["-i", path]
+            return result
+
+        def _generate_output_options(inputs):
+            commands = []
+            commands += _add_metadata(self, "")  # global metadata
+            o = 0  # output specifier index
+            for i, inpt in enumerate(inputs):  # i is input specifier
+                # every inpt is an object:
+                # - 'self' Container
+                # - 'other' Container's Stream
+                # - 'outer' Stream
+                if isinstance(inpt, Container) and inpt is not self:
+                    raise EOFError  # TODO Error
+                if inpt is self:  # inpt is 'self' Contaier
+                    for stream in filter(lambda x: x.container is self, inpt.streams):
+                        commands += stream._make_output_commands(i, o, self.extention)
+                else:  # inpt is 'other' Container's Stream OR 'outer' Stream
+                    commands += inpt._make_output_commands(i, o, self.extention)
+                o += 1
+            return commands
+
+        def _add_metadata(x, output_specifier):
+            result = []
+            for key, value in x.metadata.items():
+                result += [f"-metadata{output_specifier}", f"{key}={value}"]
+            return result
+
+        inputs = [self] + [s for s in self.streams if not s.inner]
+        call = []
+        call += ffmpeg_command
+        call += _input_paths_and_their_options(inputs)
+        call += _generate_output_options(inputs)
+
+        if self.path == self._default_path:
+            raise RuntimeError  # TODO Error
+        
+        call += [self.path]
+    
+        # print(call)
+
+        return sp.call(call)
