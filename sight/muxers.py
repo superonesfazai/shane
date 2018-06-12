750718112fa2d19f5ba6c652089d11e7c7ab505c
dmkskn <dmksknn@gmail.com>
2018-06-12 08:51:05 +0300

    add muxers

---
+++ sight/muxers.py
@@ -0,0 +1,13 @@
+
+def mkv(container):
+    """Changes the `container` format to the M4V format."""
+    container.extention = '.mkv'
+    container.save()
+    return container
+
+def m4v(container):
+    """Changes the `container` format to the M4V format."""
+    container.extention = '.m4v'
+    container.save()
+    return container
+
