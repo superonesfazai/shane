4788968ab3e51b2f9e75e9ecc7f4563daa37262e
dmkskn <dmksknn@gmail.com>
2018-06-26 15:38:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,50 @@
-from setuptools import setup
+import re
+import sys
+from setuptools import setup, find_packages
+
+assert sys.version_info >= (3, 6, 0), 'sight requires Python 3.6+'
+from pathlib import Path
+
+
+BASE = Path(__file__).parent
+NAME = 'sight'
+
+
+def get_long_description() -> str:
+    with open(BASE / 'README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def get_version() -> str:
+    with open(BASE / NAME / '__version__.py', 'r', encoding='utf-8') as f:
+        match = re.search(r'__version__\s+=\s+(?P<version>.*)', f.read())
+        version = match.group('version')
+        return eval(version)
 
 
 setup(
-    name='sight',    
-    py_modules=['sight'],
+    name=NAME,
+    version=get_version(),
+    description= 'A simple way to manage video files.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Dima Koskin',
+    author_email='dmksknn@gmail.com',
+    python_requires='>=3.6',
+    url='https://github.com/dmkskn/sight',
+    packages=find_packages(),
+    license='MIT',
+    keywords='video metadata converting muxing ffmpeg',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Multimedia',
+        'Topic :: Multimedia :: Video',
+        'Topic :: Multimedia :: Video :: Conversion',
+    ],
     )
